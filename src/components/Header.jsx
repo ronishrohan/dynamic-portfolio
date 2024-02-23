5785fb533747788a557f802d60b9b64de8eeaf34
Ronish <ronish.rohan@gmail.com>
2024-02-23 17:24:42 +0530

    made header title a link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import NavLink from "./header/NavLink";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header className="w-full fixed h-12 z-50 text-lg font-bold bg-header text-header-text flex justify-between items-center">
-      <span className="sm:ml-4 lg:flex  hidden">RONISH ROHAN</span>
+      <Link to="/" className="sm:ml-4 lg:flex  hidden">RONISH ROHAN</Link>
       <div className="h-full flex items-center">
         <NavLink to="/">HOME</NavLink>
         <NavLink to="/work">WORK</NavLink>
